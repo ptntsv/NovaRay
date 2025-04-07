e60eef0b165a84a66efe02685c387f182805d01d
Anqeliccom <hamidulinaanzelika462@gmail.com>
2025-04-07 12:38:51 +0700

    latest version added

--- src/utils/sphere.hpp
+++ src/utils/sphere.hpp
@@ -3,16 +3,17 @@
 #include "hittable.hpp"
 #include "ray.hpp"
 #include "vec3.hpp"
-#include <iostream>
 
 class sphere : public hittable {
-    point3 center;
+    point3 center1;
+    vec3 center_vec;
+    bool is_moving = false;
     double r;
     material* mat;
 
 public:
     bool hit(const ray& ray, interval tint, hit_record& record) override {
-        // std::cout << "SPHERE HIT\n";
+        point3 center = is_moving ? center1 + center_vec * ray.time() : center1;
         vec3 oc = center - ray.origin();
         vec3 d = ray.direction();
         double a = d.length_squared();
@@ -34,9 +35,19 @@ public:
         record.mat = mat;
         return true;
     }
-    sphere(const point3& center, const double& r, material* mat)
-        : center(center), r(std::fmax(0, r)), mat(mat) {
-        hitbox = aabb{center - r, center + r};
+
+    sphere(const point3& center, const double& r, material* material)
+        : center1(center), center_vec(0,0,0), r(std::fmax(0, r)),
+          mat(material), is_moving(false) {
+        hitbox = aabb{center1 - r, center1 + r};
+    }
+
+    sphere(const point3& center1, const point3& center2, const double& r, material* material)
+        : center1(center1), center_vec(center2 - center1), r(std::fmax(0, r)),
+          mat(material), is_moving(true) {
+        aabb box1(center1 - r, center1 + r);
+        aabb box2(center2 - r, center2 + r);
+        hitbox = aabb(box1, box2);
     }
     ~sphere() { delete mat; }
 };
