b61c82cd6ccc8eb260e8427f2d54427bcc576678
Anqeliccom <hamidulinaanzelika462@gmail.com>
2025-04-07 12:38:51 +0700

    latest version added

--- src/utils/sphere.hpp
+++ src/utils/sphere.hpp
@@ -5,7 +5,7 @@
 #include "vec3.hpp"
 
 class sphere : public hittable {
-    point3 center;
+    point3 center1;
     vec3 center_vec;
     bool is_moving = false;
     double r;
@@ -13,8 +13,7 @@ class sphere : public hittable {
 
 public:
     bool hit(const ray& ray, interval tint, hit_record& record) override {
-        point3 actual_center =
-            is_moving ? center + center_vec * ray.time() : center;
+        point3 center = is_moving ? center1 + center_vec * ray.time() : center1;
         vec3 oc = center - ray.origin();
         vec3 d = ray.direction();
         double a = d.length_squared();
@@ -36,17 +35,16 @@ public:
         record.mat = mat;
         return true;
     }
-    sphere(const point3& center, const double& r, material* mat)
-        : center(center), center_vec{0, 0, 0}, r(std::fmax(0, r)), mat(mat) {
-        hitbox = aabb{center - r, center + r};
+
+    sphere(const point3& center, const double& r, material* material)
+        : center1(center), center_vec(0,0,0), r(std::fmax(0, r)),
+          mat(material), is_moving(false) {
+        hitbox = aabb{center1 - r, center1 + r};
     }
-    sphere(const point3& center1, const point3& center2, const double& r,
-           material* material)
-        : center(center1),
-          center_vec(center2 - center1),
-          r(std::fmax(0, r)),
-          mat(material),
-          is_moving(true) {
+
+    sphere(const point3& center1, const point3& center2, const double& r, material* material)
+        : center1(center1), center_vec(center2 - center1), r(std::fmax(0, r)),
+          mat(material), is_moving(true) {
         aabb box1(center1 - r, center1 + r);
         aabb box2(center2 - r, center2 + r);
         hitbox = aabb(box1, box2);
